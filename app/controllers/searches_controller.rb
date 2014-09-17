0ba76bdc88674b920f58ec6021853b9731a8c28b
Ricky Bacci <brownieelf175@gmail.com>
2014-09-17 15:44:38 -0400

    fix validation and popups

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,4 @@
 class SearchesController < ApplicationController
-  #before_filter :check_for_valid_container_number
 
 
   def index
@@ -9,35 +8,16 @@ class SearchesController < ApplicationController
 
     @container = params[:container_planned].to_s\
 
-    unless @container.match(/^[a-zA-Z]{4}\d{7}$/)
-      #flash[:alert] = "Invalid container number entered!"
-      render 'searches/index'
-      flash[:alert] = "Invalid container number entered!"
-
+    if @container.match(/^[a-zA-Z]{4}\d{7}$/)
+      ## SELECT
+      fields = 'id, revenue_code_id, status, ordered_date, preloaded'
+      ## WHERE                                               
+      criteria = "container_planned = ? AND (revenue_code_id = 'CES' AND status = 'P')"
+      @search = Order.select(fields).find_by(criteria, @container)
+    else
+      flash[:notice] = "Please enter a valid container number"
+      redirect_to searches_index_path
     end
-
-
-    ## SELECT
-      #fields = 'id, revenue_code_id, status, ordered_date, temperature_min'
-    fields = 'id, revenue_code_id, status, ordered_date, preloaded'
-    ## where                                               
-    criteria = "container_planned = ? AND (revenue_code_id = 'CES' AND status = 'P')"
-    
-    @search = Order.select(fields).find_by(criteria, @container)
-  
   end
-
-
-
-
-  # private
-
-  # def check_for_valid_container_number
-  #   unless @container.match(/^[a-zA-Z]{4}\d{7}$/)
-  #     flash[:alert] = "Invalid container number entered!"
-  #     redirect_to 'searches/index'
-  #   end
-  # end
-
 end
 
